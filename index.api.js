f5f437b1f21bb54ced662946c0ffd80f6911201d
infochibafukushi-dotcom <info.chibafukushi@gmail.com>
2026-03-18 23:57:59 +0900

    Update index.api.js

--- index.api.js
+++ index.api.js
@@ -1,5 +1,5 @@
 const ADMIN_ICON_FILE_ID = '1a0QB8ei00w_lSfL4PnF_xuEFUC2JP6FW';
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxClbXJ5WPUGq2guqTk_WtzhRYL021WMWdieF4_0VV9wR02MnZTI8CSwWFYLGPzyf7j/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyyxvXGJLXGcyTwrtFvMLrWfakbDbnmQ5qdwwOCnDumkBvlzJNaje8bqvYeUnwF70q8yA/exec";
 const ADMIN_PAGE_URL = "admin.html";
 
 function toast(msg='通信エラー', ms=2200){
